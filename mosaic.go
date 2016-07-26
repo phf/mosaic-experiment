e16f6a6cd20e99fba5ccc050f552605a63b8df94
Peter Froehlich <peter.hans.froehlich@gmail.com>
2016-07-26 04:18:35 -0400

    First function!

--- mosaic.go
+++ mosaic.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	"os"
 )
+
+func loadImage(path string) (image.Image, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
 
 func main() {
 	fmt.Println("Mosaic experiment is experimental!")
+	img, _ := loadImage("hm.jpg")
+	fmt.Println(img)
 }
